9a658614bf80c9c2a72ba122fd25b47c21926123
Dhairya Gupta <dhairyag23@gmail.com>
2022-11-14 17:22:42 -0600

    Just an update to force vercel deployment

--- server/bin/firebase.js
+++ server/bin/firebase.js
@@ -2,5 +2,6 @@ import { initializeApp } from "firebase-admin/app";
 
 // initialize firebase admin, uses environment variables
 const firebaseApp = initializeApp();
+console.log(firebaseApp.name);
 
 export default firebaseApp;
