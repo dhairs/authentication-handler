cd946e705b75f73b160152fa91ffbb61ca35161c
Dhairya Gupta <dhairyag23@gmail.com>
2022-11-14 18:14:09 -0600

    plewase

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/auth", function (req, res, next) {
   res.json({
     error: "Cannot GET, please return to https://www.guptadhairya.com",
   });
